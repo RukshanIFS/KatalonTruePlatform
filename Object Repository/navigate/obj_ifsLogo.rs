96b29e6ad7d9ccb789c481f22b30ae6193ee3f25
RukshanIFS <rukshan.liyanage@ifs.com>
2026-03-10 18:56:10 +0530

    Add files via upload

---
+++ Object Repository/navigate/obj_ifsLogo.rs
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description>------------------------------------------------
+ Date       Sign        History
+---------  --------   ------------------------
+ YYMMDD  UserID  Details
+-----------------------------------------------</description>
+   <name>obj_ifsLogo</name>
+   <tag></tag>
+   <elementGuidId>aa5a6f2a-ab5c-4231-a227-fe2caadbc97a</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[contains(@class,'ifs-logo')]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
